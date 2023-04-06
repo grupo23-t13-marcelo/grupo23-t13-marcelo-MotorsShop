a5b9e3b7327b7f5479cb5e5f49467f74cd1ce5f8
João Pedro Lima <100230592+jotapeDeJotape@users.noreply.github.com>
2023-04-06 11:27:57 -0300

    Delete front directory

--- front/src/styles/theme.ts
+++
@@ -1,9 +0,0 @@
-import "@fontsource/inter"
-import { extendTheme } from '@chakra-ui/react'
-//arquivo para estilizar o chakra caso necessário
-
-const theme = extendTheme({
-
-})
-
-export default theme

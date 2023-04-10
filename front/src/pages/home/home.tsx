493c0a2606301193f4cdf0717e8fd179cf7df60f
Luan rodrigues <rodrigues.luan20@gmail.com>
2023-04-10 18:56:11 -0300

    feat: Filter components created and added on HomePage

--- front/src/pages/home/home.tsx
+++ front/src/pages/home/home.tsx
@@ -1,14 +1,10 @@
-import { Heading, Flex, Box, Text, Stack, Center, flexbox, UnorderedList, ListItem, Badge, Image, Button, Input, Link, useMediaQuery, Modal } from "@chakra-ui/react"
-import { useState } from "react";
+import { Heading, Flex, Box, Text, Stack, UnorderedList, Button } from "@chakra-ui/react"
 import homeCover from "../../assets/home_cover.png"
+import FilterType from "../../components/adFilter/FilterType";
 import { CardCars } from "../../components/commons/Card"
 
 
 const HomePage = () => {
-
-    const [filterChange] = useMediaQuery("(max-width: 768px)");
-    const [filterModal, setFilterModal] = useState<boolean>(false)
-    const onClose = () => setFilterModal(false)
 
    return (
     <Box>
@@ -79,170 +75,7 @@ const HomePage = () => {
                 <CardCars/>
                 <CardCars/>
             </UnorderedList>
-            <Box> 
-                {filterChange ? (
-                    <Stack alignItems={"center"}>
-                        <Button display={filterModal ? "none" : "inline"} variant={"button-sender"} w={"90%"} maxW={"320px"} margin={"50px 0 20px 0"} textColor={"#ffffff"} onClick={() => (setFilterModal(true), window.scrollTo({ top: 0, behavior: "smooth" }))}>Filtros</Button>
-                        {filterModal ? (
-                            <Box 
-                                width={"100%"}
-                                display={"flex"}
-                                flexDirection={"column"}
-                                backgroundColor={"#ffffff"}
-                                position={"absolute"} 
-                                zIndex={20} 
-                                top={"65px"} 
-                                left={0}
-                                padding={"12px"}
-                                alignItems={"center"}
-                            >
-                                <Flex width={"100%"} justify={"space-between"}>
-                                    <Text marginLeft={"5px"}>Filtro</Text>
-                                    <Button variant={"outline-1"} border={"none"} marginRight={"5px"} onClick={() => setFilterModal(false)}>X</Button>
-                                </Flex>
-                                <Stack marginBottom={"20px"} gap={"12px"} w={"100%"}>
-                                    <Stack>
-                                        <Heading>Marca</Heading>
-                                        <Link color={"gray.600"}>Porche</Link>
-                                        <Link color={"gray.600"}>Fiat</Link>
-                                        <Link color={"gray.600"}>Ford</Link>
-                                        <Link color={"gray.600"}>Honda</Link>
-                                    </Stack>
-                                    <Stack>
-                                        <Heading>Modelo</Heading>
-                                        <Link color={"gray.600"}>Azul</Link>
-                                        <Link color={"gray.600"}>Azul</Link>
-                                        <Link color={"gray.600"}>Azul</Link>
-                                        <Link color={"gray.600"}>Azul</Link>
-                                    </Stack>
-                                    <Stack>
-                                        <Heading>Ano</Heading>
-                                        <Link color={"gray.600"}>2020</Link>
-                                        <Link color={"gray.600"}>2020</Link>
-                                        <Link color={"gray.600"}>2020</Link>
-                                        <Link color={"gray.600"}>2020</Link>
-                                    </Stack>
-                                    <Stack>
-                                        <Heading>Combustível</Heading>
-                                        <Link color={"gray.600"}>Diesel</Link>
-                                        <Link color={"gray.600"}>Diesel</Link>
-                                        <Link color={"gray.600"}>Diesel</Link>
-                                        <Link color={"gray.600"}>Diesel</Link>
-                                    </Stack>
-                                    <Stack>
-                                        <Heading>Km</Heading>
-                                        <Flex justify={"center"} gap={"10px"}>
-                                            <Input width={"40%"} placeholder="Mínimo" />
-                                            <Input width={"40%"}  placeholder="Máximo" />
-                                        </Flex>
-                                    </Stack>
-                                    <Stack>
-                                        <Heading>Preço</Heading>
-                                        <Flex justify={"center"} gap={"10px"}>
-                                            <Input width={"40%"} placeholder="Mínimo" />
-                                            <Input width={"40%"}  placeholder="Máximo" />
-                                        </Flex>
-                                    </Stack>
-                                </Stack>
-                                <Button variant={"button-sender"} textColor={"#ffffff"} w={"90%"} maxW={"320px"} margin={"10px 0"} onClick={() => setFilterModal(false)}>Ver anúncios</Button>
-                            </Box>
-                        ) : (
-                            <Stack display={"none"}>
-                                <Stack>
-                                    <Heading>Marca</Heading>
-                                    <Link color={"gray.600"}>Porche</Link>
-                                    <Link color={"gray.600"}>Fiat</Link>
-                                    <Link color={"gray.600"}>Ford</Link>
-                                    <Link color={"gray.600"}>Honda</Link>
-                                </Stack>
-                                <Stack>
-                                    <Heading>Modelo</Heading>
-                                    <Link color={"gray.600"}>Azul</Link>
-                                    <Link color={"gray.600"}>Azul</Link>
-                                    <Link color={"gray.600"}>Azul</Link>
-                                    <Link color={"gray.600"}>Azul</Link>
-                                </Stack>
-                                <Stack>
-                                    <Heading>Ano</Heading>
-                                    <Link color={"gray.600"}>2020</Link>
-                                    <Link color={"gray.600"}>2020</Link>
-                                    <Link color={"gray.600"}>2020</Link>
-                                    <Link color={"gray.600"}>2020</Link>
-                                </Stack>
-                                <Stack>
-                                    <Heading>Combustível</Heading>
-                                    <Link color={"gray.600"}>Diesel</Link>
-                                    <Link color={"gray.600"}>Diesel</Link>
-                                    <Link color={"gray.600"}>Diesel</Link>
-                                    <Link color={"gray.600"}>Diesel</Link>
-                                </Stack>
-                                <Stack>
-                                    <Heading>Km</Heading>
-                                    <Flex justify={"center"} gap={"10px"}>
-                                        <Input width={"120px"} placeholder="Mínimo" />
-                                        <Input width={"120px"}  placeholder="Máximo" />
-                                    </Flex>
-                                </Stack>
-                                <Stack>
-                                    <Heading>Preço</Heading>
-                                    <Flex justify={"center"} gap={"10px"}>
-                                        <Input width={"120px"} placeholder="Mínimo" />
-                                        <Input width={"120px"}  placeholder="Máximo" />
-                                    </Flex>
-                                </Stack>
-                            </Stack>
-                        )}
-                    </Stack>
-                ) : (
-                    <Stack alignItems={"center"}>
-                        <Stack gap={"12px"} marginBottom={"15px"}>
-                            <Stack>
-                                <Heading>Marca</Heading>
-                                <Link color={"gray.600"}>Porche</Link>
-                                <Link color={"gray.600"}>Fiat</Link>
-                                <Link color={"gray.600"}>Ford</Link>
-                                <Link color={"gray.600"}>Honda</Link>
-                            </Stack>
-                            <Stack>
-                                <Heading>Modelo</Heading>
-                                <Link color={"gray.600"}>Azul</Link>
-                                <Link color={"gray.600"}>Azul</Link>
-                                <Link color={"gray.600"}>Azul</Link>
-                                <Link color={"gray.600"}>Azul</Link>
-                            </Stack>
-                            <Stack>
-                                <Heading>Ano</Heading>
-                                <Link color={"gray.600"}>2020</Link>
-                                <Link color={"gray.600"}>2020</Link>
-                                <Link color={"gray.600"}>2020</Link>
-                                <Link color={"gray.600"}>2020</Link>
-                            </Stack>
-                            <Stack>
-                                <Heading>Combustível</Heading>
-                                <Link color={"gray.600"}>Diesel</Link>
-                                <Link color={"gray.600"}>Diesel</Link>
-                                <Link color={"gray.600"}>Diesel</Link>
-                                <Link color={"gray.600"}>Diesel</Link>
-                            </Stack>
-                            <Stack>
-                                <Heading>Km</Heading>
-                                <Flex justify={"center"} gap={"10px"}>
-                                    <Input width={"120px"} placeholder="Mínimo" />
-                                    <Input width={"120px"}  placeholder="Máximo" />
-                                </Flex>
-                            </Stack>
-                            <Stack>
-                                <Heading>Preço</Heading>
-                                <Flex justify={"center"} gap={"10px"}>
-                                    <Input width={"120px"} placeholder="Mínimo" />
-                                    <Input width={"120px"}  placeholder="Máximo" />
-                                </Flex>
-                            </Stack>
-                        </Stack>
-                        <Button w={"100%"} variant={"button-sender"} color={"#ffffff"}>Limpar filtros</Button>
-                    </Stack>
-                )}
-            </Box>
+            <FilterType/>
         </Box>
         <Box 
             width={"100%"} 
@@ -258,7 +91,14 @@ const HomePage = () => {
                 <Text color={"gray.600"}>de</Text>
                 <Text color={"gray.600"}>2</Text>
             </Flex>
-            <Button display={"flex"} width={"100px"} backgroundColor={"transparent"} color={"brand1"}>Seguinte &gt;</Button>
+            <Button 
+                display={"flex"} 
+                width={"100px"} 
+                backgroundColor={"transparent"} 
+                color={"brand1"}
+            >
+                Seguinte &gt;
+            </Button>
         </Box>
     </Box>
    ) 

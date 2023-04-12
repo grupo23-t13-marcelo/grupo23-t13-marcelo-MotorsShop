42aeec491f1796cb56a4e966b5a019b7d0a39776
LucasR <lrigon2904@gmail.com>
2023-04-11 22:15:19 -0300

    fix: button comment / price / comment suggestions

--- front/src/pages/adsDetail/components/mainInfo.tsx
+++ front/src/pages/adsDetail/components/mainInfo.tsx
@@ -13,7 +13,7 @@ export const adMainInfo = (adToShow: any) => {
                     <Heading marginBottom={10} as='h2' size={'md'}>
                         {`${adToShow.brand} ${adToShow.model}`}
                     </Heading>
-                    <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                    <Box display={'flex'} flexDirection={['column', 'row']} justifyContent={'space-between'} alignItems={['flex-start', 'center']} gap={5}>
                         <Box display={'flex'} gap={3} alignItems={'center'}>
                             <Button h={'30px'} backgroundColor={'brand4'} color={'brand2'}>{adToShow.year}</Button>
                             <Button h={'30px'} backgroundColor={'brand4'} color={'brand2'}>{adToShow.mileage}</Button>

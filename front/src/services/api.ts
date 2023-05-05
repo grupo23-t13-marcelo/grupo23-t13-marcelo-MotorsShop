a6baf8e0e37987d30de99da5dcff0b7fb9fae110
João Pedro Lima <100230592+jotapeDeJotape@users.noreply.github.com>
2023-05-05 12:47:30 -0300

    feat: add render link

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "https://motorshopwebservice.onrender.com/",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json"

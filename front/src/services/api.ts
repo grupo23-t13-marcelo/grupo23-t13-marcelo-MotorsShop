33eb769dd4c851dfde32d8d68b3ade271c91477b
João Pedro Lima <100230592+jotapeDeJotape@users.noreply.github.com>
2023-05-09 12:13:36 -0300

    fix: vercel api

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,17 +1,12 @@
 import axios from "axios"
 
 export const api = axios.create({
-    // baseURL: "https://motorshopwebservice.onrender.com/",
-    // timeout: 10000,
-    // headers: {
-    //     "Content-Type": "application/json"
-    // }
+     baseURL: "https://motorshopwebservice.onrender.com/",
+     timeout: 10000,
+     headers: {
+         "Content-Type": "application/json"
+     }
 
-    baseURL: "http://localhost:3000/",
-    timeout: 10000,
-    headers: {
-        "Content-Type": "application/json"
-    }
 })
 
 export const karsApi = axios.create({
